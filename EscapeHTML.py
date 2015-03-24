7aa104ef52831a32f1e9860cbff2ac2d08b0924e
lgmclaughlin <lgmclaughlin905@gmail.com>
2015-03-24 09:25:27 -0700

    Commit Python code for plugin.

---
+++ EscapeHTML.py
@@ -0,0 +1,25 @@
+import sublime, sublime_plugin
+
+class EscapeCommand(sublime_plugin.TextCommand):
+    # characters to check for
+    escapeChars = {
+        '\'' : '&apos;',
+        '\"' : '&quot;',
+        '&'  : '&amp;',
+        '<'  : '&lt;',
+        '>'  : '&gt;'
+    }
+
+    def run(self, edit):
+        # Get all selected text
+        sel = self.view.sel()
+        # Loop through each region
+        for region in sel:
+            if len(region) > 0:
+                arr = list(self.view.substr(region))
+                # Loop through the string in array form and escape HTML
+                for i, char in enumerate(arr):
+                    if char in self.escapeChars:
+                        arr[i] = self.escapeChars[char];
+                # Replace the selection with transformed text
+                self.view.replace(edit, region, ''.join(arr))
